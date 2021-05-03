bd46fe5e377c9fe2525ef923a7cbebf89401e5ce
deepdhar <dhar2017.slg@gmail.com>
2021-05-03 22:39:17 +0530

    added members data

--- app/src/main/java/com/example/android/hackclubapp/MembersFragment.java
+++ app/src/main/java/com/example/android/hackclubapp/MembersFragment.java
@@ -29,10 +29,16 @@ public class MembersFragment extends Fragment {
 
         Members[] members = new Members[] {
                 //add members here
-                new Members("Deep Dhar", "Leader", R.drawable.sample_image, "deep-dhar/", "deepdhar", "deepdhar2000"),
+                new Members("Deep Dhar", "Leader", R.drawable.deep_dhar, "deep-dhar/", "deepdhar", "deepdhar2000"),
                 new Members("Nivedita Prasad", "Co-Lead", R.drawable.nivi_sample, "nivedita-prasad-706719194/", "Nivedita-coder", "NiveditaPrasa15"),
                 new Members("Alok Kumar Sah", "Co-Lead", R.drawable.alokx_sample, "alok-kumar-987b4b190/", "alokkumax", "alokkumax"),
+                new Members("Debajit Mallick", "Core Team Member", R.drawable.debajit_mallick, "debajit-mallick/", "debajit13", "mallickdebajit"),
+                new Members("Subhrnail Sarkar", "Core Team Member", R.drawable.subhranil_sample, "subhranil-sarkar/", "", ""),
+                new Members("Sayantani Deb", "Core Team Member", R.drawable.sayantani_deb, "sayantani-deb-035794200/", "SayantaniDeb", "SayantaniDeb2"),
                 new Members("Soumwadeep Guha", "Core Team Member", R.drawable.soumwadeep_sample, "soumwadeep-guha-5b60031bb/", "soumwadeep", "soumwadeep"),
+                new Members("Abhilasha Gupta", "Core Team Member", R.drawable.abhilasha_gupta, "abhilasha-gupta-829ba7190/", "AbhilashaGupta16", "Abhilasha_4sq"),
+                new Members("Aditya Singh", "Core Team Member", R.drawable.aditya_singh, "aditya-singh-257852194/", "", "ask2addi"),
+                new Members("Sayantani Sarkar", "Core Team Member", R.drawable.sayantani_sarkar, "sayantani-sarkar-026199200/", "sayantani9841", "Sayanta79969281")
         };
 
         MembersAdapter adapter = new MembersAdapter(members, getContext());
